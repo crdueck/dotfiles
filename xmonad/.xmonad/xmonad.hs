f6907c1226a248d2338a7a4393f0c767488864a8
Chris Dueck <crdueck@uwaterloo.ca>
2021-09-30 14:13:56 -0400

    xmonad: update xmonad.hs

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,132 +2,158 @@
 
 import XMonad
 import XMonad.Actions.CycleWS
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.FadeWindows
+import XMonad.Actions.CopyWindow
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers (isDialog, doCenterFloat)
 import XMonad.Layout.Fullscreen
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Reflect
 import XMonad.Layout.ResizableTile
-import XMonad.Layout.TwoPane
+import XMonad.Layout.Simplest
+import XMonad.Layout.SubLayouts hiding (subTabbed)
+import XMonad.Layout.Tabbed
+import XMonad.Layout.TwoPanePersistent
+import XMonad.Layout.WindowNavigation
 import XMonad.Util.Cursor
-import XMonad.Util.Run
 
+import Data.List (isInfixOf)
+import Data.Map (Map, fromList)
+import Graphics.X11.ExtraTypes.XF86
 import System.Exit (exitSuccess)
-import System.IO (Handle)
 
-import qualified Data.Map as M
+import XMonad.StackSet (Stack(..))
 import qualified XMonad.StackSet as W
 
-rofi :: String
-rofi = "rofi -show run -hide-scrollbar"
-
-tmux :: String
-tmux = "urxvtc -e /bin/sh -c '(tmux -q has && tmux att -t 0) || tmux new'"
-
 main :: IO ()
-main = do
-    xmobar <- spawnPipe "xmobar -A 204 ~/.xmonad/xmobarrc"
-    xmonad $ def
-        { clickJustFocuses   = False
-        , focusFollowsMouse  = False
-        , focusedBorderColor = "#101010"
-        , handleEventHook    = docksEventHook
-        , keys               = xKeys
-        , layoutHook         = xLayout
-        , logHook            = xLogHook xmobar
-        , manageHook         = manageDocks
-        , normalBorderColor  = "#101010"
-        , workspaces         = xWorkspaces
-        , terminal           = "urxvtc"
-        }
-
-xLogHook :: Handle -> X ()
-xLogHook hdl = dynamicLogWithPP def
-    { ppCurrent         = xmobarColor "#7cafc2" "#101010" . pad
-    , ppHidden          = xmobarColor "#f8f8f8" "#101010" . pad
-    , ppHiddenNoWindows = xmobarColor "#383838" "#101010" . pad
-    , ppUrgent          = xmobarColor "#ab4642" "#101010" . pad
-    , ppSep             = xmobarColor "#757575" "#101010" ""
-    , ppWsSep           = ""
-    , ppTitle           = xmobarColor "#f8f8f8" "#101010" . takeNWords 5
-    , ppLayout          = const ""
-    , ppOrder           = take 1
-    , ppOutput          = hPutStrLn hdl
+main = xmonad . fullscreenSupport . docks $ ewmh def
+    { clickJustFocuses   = False
+    , focusFollowsMouse  = False
+    , focusedBorderColor = "#aeddff"
+    , normalBorderColor  = "#181818"
+    , keys               = keybindings
+    , layoutHook         = layoutHooks
+    , manageHook         = manageHooks
+    , startupHook        = startupHooks
+    , workspaces         = ["1"]
+    , terminal           = "alacritty"
     }
 
-takeNWords :: Int -> String -> String
-takeNWords n = unwords . take n . words
+manageHooks = isDialog --> doCenterFloat
 
-xWorkspaces :: [String]
-xWorkspaces = map fontawesome
-    [ "\xf269" -- web
-    , "\xf121" -- dev
-    , "\xf19d" -- edu
-    , "\xf0e0" -- mail
-    , "\xf11b" -- game
-    ] where fontawesome = wrap "<fn=1>" "</fn>"
+layoutHooks = smartBorders . mkToggle1 REFLECTX $
+    avoidStruts (tallTab ||| twoPane) ||| fullscreenFull Full
+    where tall = ResizableTall 1 (3/100) (1/2) []
+          tallTab = windowNavigation (subtabbed tall)
+          twoPane = TwoPanePersistent Nothing (3/100) (1/2)
 
-xLayout = avoidStruts resizableTall ||| avoidStruts twoPane ||| fullscreenFull Full
-    where resizableTall = ResizableTall 1 (3/100) (3/5) []
-          twoPane = TwoPane (3/100) (1/2)
+subtabbed = addTabs shrinkText tabConfig . subLayout [] Simplest
+    where tabConfig = def
+              { fontName            = "xft:Hack Nerd Font"
+              , activeColor         = "#aeddff"
+              , activeTextColor     = "#aeddff"
+              , activeBorderColor   = "#aeddff"
+              , inactiveColor       = "#1e282d"
+              , inactiveTextColor   = "#1e282d"
+              , inactiveBorderColor = "#1e282d"
+              , urgentTextColor     = "#ff262b"
+              }
+
+startupHooks :: X ()
+startupHooks = do
+    spawn "~/bin/polybar.sh"
+    setDefaultCursor xC_left_ptr
+
+-- cycle focus up/down off master
+focusDown = W.modify' $ rotateStack (\l -> [last l] ++ (init l))
+focusUp   = W.modify' $ rotateStack (\l -> (tail l) ++ [head l])
+
+rotateStack :: ([a] -> [a]) -> Stack a -> Stack a
+rotateStack _ (Stack w [] []) = Stack w [] []
+rotateStack _ (Stack w [] (x:xs)) = Stack x [w] xs
+rotateStack f (Stack w ls rs) = Stack w' (reverse ls') rs'
+    where (master:subs) = reverse ls ++ w : rs
+          (ls', w':rs') = splitAt (length ls) (master:f subs)
+
+ifeLayout :: String -> a -> a -> X a
+ifeLayout label t f = do
+    layout <- gets (W.layout . W.workspace . W.current . windowset)
+    return $ if isInfixOf label (description layout) then t else f
 
 winMask :: KeyMask
 winMask = mod4Mask
 
-xKeys :: XConfig l -> M.Map (KeyMask, KeySym) (X ())
-xKeys XConfig{..} = M.fromList $
-    [
+ctrlMask :: KeyMask
+ctrlMask = controlMask
+
+keybindings :: XConfig l -> Map (KeyMask, KeySym) (X ())
+keybindings XConfig{..} = Data.Map.fromList $
+    [ ((modMask,               xK_p        ), spawn "rofi -show run")
+    , ((modMask,               xK_Tab      ), spawn "rofi -show window")
+    , ((modMask .|. shiftMask, xK_Return   ), spawn terminal)
+    -- unmerge windows out of sublayout
+    , ((modMask,               xK_g        ), withFocused (sendMessage . UnMerge))
+    -- merge all windows into sublayout
+    , ((modMask .|. shiftMask, xK_g        ), withFocused (sendMessage . MergeAll))
     -- close focused window
-      ((modMask .|. shiftMask, xK_c        ), kill)
+    , ((modMask .|. shiftMask, xK_q        ), kill1)
     -- rotate through the available layout algorithms
     , ((modMask,               xK_space    ), sendMessage NextLayout)
+    -- toggle horizontal layout mirroring
+    , ((controlMask,           xK_space    ), sendMessage (Toggle REFLECTX))
     -- move focus to the next window
-    , ((modMask,               xK_t        ), windows W.focusDown)
-    -- move focus to the previous window
-    , ((modMask,               xK_n        ), windows W.focusUp)
-    -- move focus to the master window
+    , ((modMask,               xK_t        ), windows =<< ifeLayout "TwoPane" focusDown W.focusDown)
+    -- move focus to the prevous window
+    , ((modMask,               xK_n        ), windows =<< ifeLayout "TwoPane" focusUp W.focusUp)
+    -- move focus to the master window;
     , ((modMask,               xK_m        ), windows W.focusMaster)
     -- swap focused window and master window
-    , ((modMask .|. shiftMask, xK_m        ), windows W.swapMaster)
+    , ((modMask,               xK_Return   ), windows W.swapMaster)
     -- swap focused window and next window
     , ((modMask .|. shiftMask, xK_t        ), windows W.swapDown)
     -- swap focused window and previous window
     , ((modMask .|. shiftMask, xK_n        ), windows W.swapUp)
+    -- pull focused window down into group
+    , ((modMask .|. ctrlMask,  xK_t        ), sendMessage (pullGroup D))
+    -- pull focused window up into group
+    , ((modMask .|. ctrlMask,  xK_n        ), sendMessage (pullGroup U))
     -- shrink the master area
-    , ((modMask,               xK_h        ), sendMessage Shrink)
+    , ((modMask,               xK_h        ), sendMessage =<< ifeLayout "ReflectX" Expand Shrink)
     -- expand the master area
-    , ((modMask,               xK_s        ), sendMessage Expand)
-    -- shrink the slave area
+    , ((modMask,               xK_s        ), sendMessage =<< ifeLayout "ReflectX" Shrink Expand)
+    -- shrink the stack area
     , ((modMask .|. shiftMask, xK_h        ), sendMessage MirrorShrink)
-    -- expand the slave area
+    -- expand the stack area
     , ((modMask .|. shiftMask, xK_s        ), sendMessage MirrorExpand)
     -- sink floating window
     , ((modMask,               xK_f        ), withFocused (windows . W.sink))
-    -- restart xmonad/xmobar
-    , ((modMask .|. shiftMask, xK_r        ), spawn "killall xmobar; xmonad --recompile && xmonad --restart")
+    -- restart xmonad
+    , ((modMask .|. shiftMask, xK_r        ), spawn "xmonad --recompile && xmonad --restart")
     -- kill xmonad and X11
-    , ((modMask .|. shiftMask, xK_q        ), liftIO exitSuccess)
+    , ((modMask .|. shiftMask, xK_Escape   ), liftIO exitSuccess)
     -- move to next workspace
-    , ((winMask,               xK_Return   ), nextWS)
+    , ((winMask,               xK_Return   ), removeEmptyWorkspaceAfter nextWS)
     -- move to previous workspace
-    , ((winMask,               xK_BackSpace), prevWS)
-    -- take window to next workspace
-    , ((winMask .|. shiftMask, xK_Return   ), shiftToNext >> nextWS)
-    -- take window to previous workspace
-    , ((winMask .|. shiftMask, xK_BackSpace), shiftToPrev >> prevWS)
+    , ((winMask,               xK_BackSpace), removeEmptyWorkspaceAfter prevWS)
+    -- bring window to next workspace
+    , ((winMask .|. shiftMask, xK_Return   ), removeEmptyWorkspaceAfter (shiftToNext >> nextWS))
+    -- bring window to previous workspace
+    , ((winMask .|. shiftMask, xK_BackSpace), removeEmptyWorkspaceAfter (shiftToPrev >> prevWS))
     -- media bindings
-    , ((0, 0x1008ff11), spawn "amixer set Master 5- toggle unmute")
-    , ((0, 0x1008ff13), spawn "amixer set Master 5+ toggle unmute")
-    , ((0, 0x1008ff14), spawn "mpc toggle")
-    , ((0, 0x1008ff16), spawn "mpc prev")
-    , ((0, 0x1008ff17), spawn "mpc next")
-    -- program bindings
-    , ((modMask, xK_comma), spawn terminal)
-    , ((modMask, xK_p), spawn rofi)
-    , ((modMask, xK_i), spawn tmux)
+    , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 5%- toggle unmute")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+ toggle unmute")
+    , ((0, xF86XK_AudioPlay), spawn "mpc toggle")
+    , ((0, xF86XK_AudioPrev), spawn "mpc prev")
+    , ((0, xF86XK_AudioNext), spawn "mpc next")
+    , ((0, xF86XK_AudioStop), spawn "mpc stop")
     ] ++
-    -- win-[1..9]: switch to workspace N
-    -- win-shift-[1..9]: move client to workspace N
-    [((m .|. winMask, k), windows (f i)) |
-        (i, k) <- zip workspaces [xK_1 .. xK_9],
-        (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    -- win-[1..9]: move to workspace N
+    -- win-ctrl-[1..9]: copy window to workspace N
+    -- win-shift-[1..9]: bring window to workspace N
+    [((winMask .|. m, k), removeEmptyWorkspaceAfter (withHiddenWorkspace f (show i))) |
+        (i, k) <- zip [1..] [xK_1..xK_9],
+        (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask), (copy, ctrlMask)]
     ]
+        where withHiddenWorkspace f i = addHiddenWorkspace i >> windows (f i)
