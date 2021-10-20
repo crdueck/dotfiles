56928069d8745f518b48c3cc4398b1a24e2b5c18
Chris Dueck <crdueck@uwaterloo.ca>
2021-10-20 14:24:51 -0400

    Add neovim config

---
+++ neovim/.config/nvim/lua/settings.lua
@@ -0,0 +1,32 @@
+local opt = vim.opt
+
+opt.hidden = true
+opt.mouse = "a"
+opt.number = true
+opt.scrolljump = 5
+opt.scrolloff = 5
+opt.shortmess = opt.shortmess + "c"
+opt.showmode = false
+opt.splitright = true
+opt.swapfile = false
+
+-- Completion
+opt.completeopt = "menuone,noselect"
+
+-- Formatting
+opt.formatoptions = "crqj"
+opt.linebreak = true
+opt.showbreak = "↲"
+opt.textwidth = 80
+opt.wrap = false
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Whitespace
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.list = true
+opt.listchars = { tab = "▷•", trail = "•" }
