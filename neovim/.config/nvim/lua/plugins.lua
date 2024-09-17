10a122aa7cd2a4ea3584e9bf50fa83c5bedeb7d3
Chris Dueck <chris.dueck@wattpad.com>
2024-09-17 16:07:11 -0400

    neovim: sync config

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -123,29 +123,6 @@ require("lazy").setup({
     },
 
     {
-          "folke/trouble.nvim",
-          opts = {},
-          cmd = "Trouble",
-          keys = {
-            {
-              "<leader>xx",
-              "<cmd>Trouble diagnostics toggle<cr>",
-              desc = "Diagnostics (Trouble)",
-            },
-            {
-              "<leader>xX",
-              "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-              desc = "Buffer Diagnostics (Trouble)",
-            },
-            {
-              "<leader>xt",
-              "<cmd>Trouble symbols toggle focus=false<cr>",
-              desc = "Symbols (Trouble)",
-            },
-        },
-    },
-
-    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
